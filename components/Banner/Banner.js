953b1ac1f8ebc6a14838c39db44276e3c178991b
AhshanHabib26 <habib.net27@gmail.com>
2022-08-07 09:32:14 +0600

    Banner Added

---
+++ components/Banner/Banner.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className=" bg-purple-100">
+      <div className=" grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="card">
+          <div className="card-body mt-10">
+            <h2 className=" text-4xl lg:text-6xl font-extrabold">
+              Your Favorite Food, <br /> Delivered Your Home
+            </h2>
+            <p className=" flex-grow-0 text-lg lg:text-xl opacity-70">
+              Food, Drinks, Medicine, Groceries Available For Delivery
+            </p>
+            <div className=" flex items-center mt-10">
+              <input
+                type="text"
+                placeholder="Enter Your Email, We Contact You!"
+                className="input input-bordered outline-0 rounded-l-md rounded-none w-full max-w-xs"
+              />
+              <button className="btn rounded-r-md rounded-none bg-gray-800">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <Image
+              width={450}
+              height={450}
+              alt="Banner"
+              src="/FoodBanner.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Banner from "../components/Banner/Banner";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
   return <div>
     <Navbar/>
+    <Banner/>
   </div>;
 }
