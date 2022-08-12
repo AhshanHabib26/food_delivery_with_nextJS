e7eb2833ab6b5e804db12cd1c4f67c8d8d476c49
AhshanHabib26 <habib.net27@gmail.com>
2022-08-12 16:59:58 +0600

    Code Fixed

--- components/Offer/Offer.js
+++ components/Offer/Offer.js
@@ -3,7 +3,7 @@ import { GiNotebook } from "react-icons/gi";
 
 const Offer = () => {
   return (
-    <div>
+    <div className=" bg-white py-5">
       <div data-aos="fade-up" data-aos-duration="3000" className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <Image
@@ -14,8 +14,8 @@ const Offer = () => {
             alt="Burger"
           />
           <div>
-            <h1 className="text-5xl font-bold mb-6">Weekly Hot Deals!</h1>
-            <p className=" text-2xl font-bold">Thai Fried Chicken Burger</p>
+            <h1 className="text-5xl text-gray-800 font-bold mb-6">Weekly Hot Deals!</h1>
+            <p className=" text-2xl text-gray-800 font-bold">Thai Fried Chicken Burger</p>
             <p className=" text-xl font-semibold mb-3">
               Price: <span className=" text-red-500">$ 80</span>
             </p>
@@ -27,27 +27,27 @@ const Offer = () => {
                 <div className="left_side">
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2">20g Bulgur Wheat</h1>
+                    <h1 className="  text-gray-600  ml-2">20g Bulgur Wheat</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2 my-2">500g Lamb Mince</h1>
+                    <h1 className=" text-gray-600  ml-2 my-2">500g Lamb Mince</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2">1 TSP Cumin</h1>
+                    <h1 className=" text-gray-600  ml-2">1 TSP Cumin</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2 my-2">1 TSP Coriander</h1>
+                    <h1 className=" text-gray-600  ml-2 my-2">1 TSP Coriander</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2">1 TSP Paprika</h1>
+                    <h1 className="  text-gray-600 ml-2">1 TSP Paprika</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2 my-2">
+                    <h1 className=" text-gray-600  ml-2 my-2">
                       1 Clove Finely Chopped Garlic
                     </h1>
                   </div>
@@ -55,23 +55,23 @@ const Offer = () => {
                 <div className="right_side">
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2">For Frying Olive Oil</h1>
+                    <h1 className=" text-gray-600  ml-2">For Frying Olive Oil</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2 my-2">Freash 4 Bun</h1>
+                    <h1 className=" text-gray-600  ml-2 my-2">Freash 4 Bun</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2">Grated Cucumber</h1>
+                    <h1 className=" text-gray-600  ml-2">Grated Cucumber</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2 my-2">150g Greek Yogurt</h1>
+                    <h1 className=" text-gray-600  ml-2 my-2">150g Greek Yogurt</h1>
                   </div>
                   <div className=" flex  items-center">
                     <GiNotebook />
-                    <h1 className=" ml-2">2 Mint</h1>
+                    <h1 className=" text-gray-600  ml-2">2 Mint</h1>
                   </div>
                 </div>
               </div>
